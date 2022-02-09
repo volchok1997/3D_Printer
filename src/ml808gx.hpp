e6396d86d61bdb59f71a21e0ac146ff2fe126982
jamiey <jmy@xjtu>
2022-02-09 00:03:55 -0500

    fix: CAN command format

--- src/ml808gx.hpp
+++ src/ml808gx.hpp
@@ -17,7 +17,7 @@ private:
 	const std::string A0  = STX+"02A02D"+ETX;           // "\x02\x30\x32\x41\x30\x32\x44\x03"
 	const std::string A2  = STX+"02A22B"+ETX;           // "\x02\x30\x32\x41\x32\x32\x42\x03"
 	const std::string EOT = "\x04";
-	const std::string CAN = STX+"0418186C"+ETX;         //"\x02""0418186C\x03";
+	const std::string CAN = STX+"021835"+ETX;         //"\x02""0418186C\x03";
     // Command
     const std::string CMD_F_RM = STX+"05RM   9C"+ETX;
     const std::string CMD_F_DI = STX+"04DI  CF"+ETX;
