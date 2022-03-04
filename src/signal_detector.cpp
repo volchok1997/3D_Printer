bd07548b7b3cf5c8271e5751269a659d0ffee9d7
jamiey <jmy@xjtu>
2022-03-03 20:54:57 -0500

    feat: generate log info when printing

--- src/signal_detector.cpp
+++ src/signal_detector.cpp
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <chrono>
+#include <ctime>
+
 
 #include <pigpio.h>
 
@@ -10,105 +13,78 @@
 
 #include "gpio_direct_register_access.hpp"
 
+const int DBG_GPIO=21;
 
-static int timeout = 0;  // 1ms
-static int status = 0;
-static uint32_t prevTick=0;
 
-static void _cb(int gpio, int level, uint32_t tick, void *user) {
-    ML808GX* dispenser = (ML808GX*)(user);
-#if 0
-    switch(level) {
-        case PI_TIMEOUT:
-            // STOP dispenser, if it's not stopped yet
-            if(status == 1)
-                //dispenser->StopDispense();
-                fprintf(stderr, "PWM stopped xxxxxx!, AT: %d us, last seen in %d us before\n", tick, tick - prevTick);
-            status = 0;
-            break;
-        case PI_HIGH:
-            // Start dispenser, if it's not started yet
-            if(status == 0) {
-                //dispenser->StartDispense();
-                // fprintf(stderr, "PWM detected HIGH !, AT: %d us, last seen in %d us before\n", tick, tick - prevTick);
-    		prevTick = tick;
-            }
-            status = 1;
-            break;
-        default:
-            break;
-    }
-    // reset GPIO
-    gpioSetWatchdog(gpio, timeout);
-#endif
-}
-
-int signalDetectorInitial(int pinX) {
+int signalDetectorInitial(int gpioX) {
     if(gpioInitialise()<0)
         return -1;
-    gpioSetMode(pinX, PI_INPUT);
-    gpioSetPullUpDown(pinX, PI_PUD_UP);
+    setup_io();
+
+    INP_GPIO(gpioX); // must use INP_GPIO before we can use OUT_GPIO
+
+    // DEBUG signal
+    INP_GPIO(DBG_GPIO);  
+    OUT_GPIO(DBG_GPIO);
+
     return 0;
 }
 
-int sampleWave(int pinX, int t, void* user){
-    int prev_tick;
+int trackWave(int gpioX, int t, void* user){
+    ML808GX* dispenser = (ML808GX*)user;
+    int prev_tick = gpioTick();
     int pinState = 0;
+    unsigned debounce=t;
     unsigned start;
     unsigned end;
     int max=0;
-    unsigned timeout=150;
 
-    setup_io();
+    auto timeStart= std::chrono::system_clock::now();
+    auto timeEnd  = std::chrono::system_clock::now();
+    std::time_t currTime = std::chrono::system_clock::to_time_t(timeEnd);
+    std::chrono::duration<double> elapsed_seconds = timeEnd-timeStart;
 
-    INP_GPIO(21); // must use INP_GPIO before we can use OUT_GPIO
-    OUT_GPIO(21);
-    
     while(1) {
-        int pin = gpioRead(pinX);
+        //int pin = gpioRead(pinX);
+        int pin = (GET_GPIO(gpioX) !=0);
         auto tick = gpioTick();
 	
-	
-	//if no change wait delay and check again. reset start tick for HIGH
-	if(pinState == pin) {
-	    if(pin == 1)
-		start = tick;
-	    gpioDelay(1);
-	    continue;
-	}
+        //if no change wait delay and check again. reset start tick for HIGH
+        if(pinState == pin) {
+            if(pin == 1)
+                start = tick;
+            gpioDelay(1);
+            continue;
+        }
     
-	//if rising edge detected start counting
-	if(pin == 1) {
-	    start = tick;
-	    if(pinState == 0) {
-		GPIO_SET = 1<<21;
-	        fprintf(stderr, "start printing\n");
-	        pinState = pin;
-	    }
-	//if LOW then check the timeout
-	}else {
-	    end = tick;
-	    if(timeout < end-start){
-		GPIO_CLR = 1<<21;
-	        fprintf(stderr, "range detected : %d us\n", end-start);
-	        max = end-start;
-	        pinState = pin;
-	    }
-	}
-	gpioDelay(1);
+        //if rising edge detected start counting
+        if(pin == 1) {
+            start = tick;
+            if(pinState == 0) {
+                dispenser->StartDispense();
+                GPIO_SET(DBG_GPIO);
+                timeStart= std::chrono::system_clock::now();
+                currTime = std::chrono::system_clock::to_time_t(timeStart);
+                fprintf(stderr, "%s : Start dispenser\n", std::ctime(&currTime));
+                pinState = pin;
+            }
+        //if LOW then check the timeout
+        }else {
+            end = tick;
+            if(debounce < end-start){
+                dispenser->StopDispense();
+                GPIO_CLR(DBG_GPIO);
+                timeEnd= std::chrono::system_clock::now();
+                currTime = std::chrono::system_clock::to_time_t(timeEnd);
+                elapsed_seconds = timeEnd-timeStart;
+                fprintf(stderr, "%s : Stop dispenser, duration %f seconds\n", std::ctime(&currTime), elapsed_seconds.count());
+                fprintf(stderr, "range detected : %d us\n", end-start);
+                max = end-start;
+                pinState = pin;
+            }
+        }
+        gpioDelay(1);
     }
 
     return 0;
-}
-
-void enableTrigger(int pinX, int t, void* dispenser) {
-    timeout = t;
-    prevTick = gpioTick();
-    sampleWave(pinX, timeout, dispenser);
-    //gpioSetWatchdog(pinX, timeout);
-    //gpioSetAlertFuncEx(pinX, _cb, dispenser);
-}
-
-void cancelTrigger(int pinX) {
-    gpioSetAlertFunc(pinX, 0);
 }
